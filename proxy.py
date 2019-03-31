bf148e61f8fdae72a28e5d48930c8879ca7ef0ff
Candace Williams <candacewilliams@users.noreply.github.com>
2019-03-31 00:40:54 -0400

    making changes to fix bug

--- proxy.py
+++ proxy.py
@@ -1,15 +1,5 @@
-"""A proxy server that forwards requests from one port to another server.
 
-To run this using Python 2.7:
-
-% python proxy.py
-
-It listens on a port (`LISTENING_PORT`, below) and forwards commands to the
-server. The server is at `SERVER_ADDRESS`:`SERVER_PORT` below.
-"""
-
-# This code uses Python 2.7. These imports make the 2.7 code feel a lot closer
-# to Python 3. (They're also good changes to the language!)
+# This code uses Python 2.7. These imports make the 2.7 code feel a lot closer# to Python 3. (They're also good changes to the language!)
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -31,9 +21,9 @@ MAX_CACHE_AGE_SEC = 60.0  # 1 minute
 
 def ForwardCommandToServer(command, server_addr, server_port):
     
-    client_socket = library.create_client_socket(server_addr, server_port)
+    client_socket = library.CreateClientSocket(server_addr, server_port)
     client_socket.send(command + '\n')
-    server_response = library.read_command(client_socket)
+    server_response = library.ReadCommand(client_socket)
     client_socket.close()
 
     return server_response + '\n'
@@ -42,7 +32,7 @@ def CheckCachedResponse(command_line, cache):
     cmd, name, text = library.ParseCommand(command_line)
     
     if cmd == "PUT":
-        cache.store_value(name,text)
+        cache.StoreValue(name,text)
 
         ForwardCommandToServer(command_line, SERVER_ADDRESS, SERVER_PORT)
     
@@ -55,15 +45,15 @@ def CheckCachedResponse(command_line, cache):
             ForwardCommandToServer(command_line, SERVER_ADDRESS, SERVER_PORT)
             
 def ProxyClientCommand(sock, server_addr, server_port, cache):
-    command_line = library.read_command(sock)
-    cmd, name, value = library.parse_command(command_line)
+    command_line = library.ReadCommand(sock)
+    cmd, name, value = library.ParseCommand(command_line)
 
     if cmd == 'PUT':
         server_response = ForwardCommandToServer(command_line, server_addr, server_port)
-        cache.store_value(name, server_response)
+        cache.StoreValue(name, server_response)
 
     elif (cmd == 'GET'):
-        server_response = cache.get_value(name)
+        server_response = cache.GetValue(name)
         if server_response == None:
             server_response = ForwardCommandToServer(command_line, server_addr, server_port)
 
@@ -86,6 +76,6 @@ def main():
         print('Received connection from %s:%d' % (address, port))
         ProxyClientCommand(client_sock, SERVER_ADDRESS, SERVER_PORT, cache)
         
-    client_sock.close()
+        client_sock.close()
     
 main()
